84628d64f48d0b4ac595fc1c265756102a82c4f9
Er3nairus <129863421+Er3nairus@users.noreply.github.com>
2023-04-16 17:39:17 +0300

    Add files via upload

---
+++ Vüvut Kitle indeksi/vucutkitle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class vucutkitle {
+
+    public static void main(String[] args) {
+
+        double boy , kilo , index;
+
+        Scanner kitle = new Scanner(System.in);
+
+        System.out.print("Boyunuzu Metre Cinsinden Giriniz : ");
+
+        boy = kitle.nextDouble();
+
+        System.out.print("Kilonuzu Giriniz : ");
+
+        kilo = kitle.nextDouble();
+
+        index = kilo / (boy * boy);
+
+        System.out.print("Kitle indeksiniz : "+ index);
+        
+    }
+    
+}
